cd7edab306fe597b39e6ba547e04873aa0bbdc28
Sanluis94 <luisadamski94@gmail.com>
2022-12-05 15:07:50 -0300

    Testing points system

--- Quiz/questions.py
+++ Quiz/questions.py
@@ -1,5 +1,14 @@
 questions = []
 
+question_0 = {
+    'question': 'In what language this quiz was made?',
+    'A': 'C++',
+    'B': 'C#',
+    'C': 'Python',
+    'D': 'Java',
+    'correct': 'C'
+}
+
 question_1 = {
     'question': 'In what language this quiz was made?',
     'A': 'C++',
@@ -9,4 +18,25 @@ question_1 = {
     'correct': 'C'
 }
 
+question_2 = {
+    'question': 'In what language this quiz was made?',
+    'A': 'C++',
+    'B': 'C#',
+    'C': 'Python',
+    'D': 'Java',
+    'correct': 'C'
+}
+
+question_3 = {
+    'question': 'In what language this quiz was made?',
+    'A': 'C++',
+    'B': 'C#',
+    'C': 'Python',
+    'D': 'Java',
+    'correct': 'C'
+}
+
+questions.append(question_0)
 questions.append(question_1)
+questions.append(question_2)
+questions.append(question_3)
--- main.py
+++
@@ -1,8 +0,0 @@
-from quiz import Quiz
-from questions import questions
-
-quiz = Quiz(0)
-
-for i in range(len(questions)):
-    question = questions[i]
-    quiz.update(question['question'],question['A'],question['B'],question['C'],question['D'],question['correct'])
